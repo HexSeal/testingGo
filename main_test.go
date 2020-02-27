ffba0b72c6d4f5682f586698f380b62874e1efd7
HexSeal <maxfin13@gmail.com>
2020-02-27 15:47:54 -0800

    Finished the testing and benchmarking tutorials at https://tutorialedge.net/golang/benchmarking-your-go-programs/

--- main_test.go
+++ main_test.go
@@ -1,24 +1,49 @@
 package main
 
 import (
-    "testing"
+	"testing"
+	"fmt"
 )
 
-func TestTableCalculate(t *testing.T) {
-    var tests = []struct {
-        input    int
-        expected int
-    }{
-        {2, 4},
-        {-1, 1},
-        {0, 2},
-        {-5, -3},
-        {99999, 100001},
-    }
+// Go test flags:
+// -v shows verbose test logs, -cover shows test coverage, -coverprofile=coverage.out shows you which files lack coverage
 
-    for _, test := range tests {
-        if output := Calculate(test.input); output != test.expected {
-            t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
-        }
+func TestTableCalculate(t *testing.T) {
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{2, 4},
+		{-1, 1},
+		{0, 2},
+		{-5, -3},
+		{99999, 100001},
+	}
+
+	for _, test := range tests {
+		if output := Calculate(test.input); output != test.expected {
+			t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
+		}
+	}
+}
+
+// go test -run=Calculate -bench=. 
+// To run both test and benchmark
+
+// go test -run=Bench -bench=.
+// To just run benchmark
+
+func benchmarkCalculate(input int, b *testing.B) {
+    for n := 0; n < b.N; n++ {
+        Calculate(input)
     }
+}
+
+func BenchmarkCalculate100(b *testing.B)         { benchmarkCalculate(100, b) }
+func BenchmarkCalculateNegative100(b *testing.B) { benchmarkCalculate(-100, b) }
+func BenchmarkCalculateNegative1(b *testing.B)   { benchmarkCalculate(-1, b) }
+
+func TestOther(t *testing.T) {
+    fmt.Println("Testing something else")
+    fmt.Println("This shouldn't run with -run=calc")
 }
